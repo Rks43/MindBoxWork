6eb0b2b3712ba1b86113c9853d84b6cba67de678
Your Name <your_email@whatever.com>
2024-10-04 10:08:12 +0300

    update

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -8,11 +8,15 @@ namespace Test
         public void testCircle()
         {
             Assert.Equal(12.57, new Ñalculator().calculateAreaCircle(2));
+            Assert.Equal(0, new Ñalculator().calculateAreaCircle(0));
+
         }
         [Fact]
         public void testTriangle()
         {
             Assert.Equal(1.98, new Ñalculator().calculateAreaTriangle(3,2,2));
+            Assert.Equal(0, new Ñalculator().calculateAreaTriangle(0, 0, 0));
+
         }
     }
 }
